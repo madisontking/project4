8f5ad1228be11e3be1492cd33d94b371b490fed4
madisontking <madisontking@yahoo.com>
2016-03-30 22:46:35 -0500

    Practice for GitHub

---
+++ Project4/src/Project4Driver.java
@@ -0,0 +1,13 @@
+
+public class Project4Driver
+{
+	
+	public static void  main(String[] args)
+	{
+		//print my name
+		String first = "Madison";
+		String second = "King";
+		System.out.println(first + " " + second);
+	}
+
+}
